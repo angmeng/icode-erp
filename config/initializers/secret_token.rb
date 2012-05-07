356e5a6b7032cf4f5dbf638669dff6b4be0ae212
sity kholijah <sitikholijahassan@gmail.com>
2012-05-07 22:25:59 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Merp::Application.config.secret_token = 'f3cc256a69c9aec8e48c51a73a98e4d74c0e6e69ea526fd4d1a3da818061b474651d3de81b5f870c3ae8298d91868f936aa9b607bbcbe4341e94afde6c80814c'
