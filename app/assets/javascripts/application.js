b38c63c23cde069f7052172c46a3b1a9aae57317
kai kean <khongkk89@gmail.com>
2013-02-06 23:49:31 +0800

    layout edit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -73,3 +73,23 @@ $(function() {
 //}
 
 //<input type=”text” name=”mytext” onKeyPress=”return disableEnterKey(event)”>
+
+
+$(document).ready(function() {
+    $("#vertical").kendoSplitter({
+        orientation: "vertical",
+        panes: [
+            { collapsible: false },
+            { collapsible: false, size: "100px" },
+            { collapsible: false, resizable: false, size: "100px" }
+        ]
+    });
+
+    $("#horizontal").kendoSplitter({
+        panes: [
+            { collapsible: true, size: "220px" },
+            { collapsible: false },
+            { collapsible: true, size: "220px" }
+        ]
+    });
+});
